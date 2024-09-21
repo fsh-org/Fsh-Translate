418fea7d9baba8d7ef740b1f75380903c93d0e35
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-22 00:47:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   Split(['.side', '.panel']);
 });
+
+function parse(con, type) {
+  if (type === 'json') return JSON.parse(con);
+  if (type === 'ftl') {
+    let obj = {};
+    con.split('\n').map(ln=>obj[ln.split(' = ')[0]]=ln.split(' = ')[1]);
+    return obj;
+  }
+  return {}
+}
