a97467ee576f271522366e5d8fd9393162795550
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-23 09:34:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -44,7 +44,10 @@ function readFile(file) {
   });
 }
 function ObjectToTree(obj, prefix) {
-  Object.keys(obj).map(k=>(typeof obj[k])==='string'?`<button>${k}</button>`:`<details><summary>${k}</summary>${ObjectToTree(obj[k]), (prefix.length?prefix+'.':'')+k}</details>`)
+  if (prefix.startsWith('.')) {
+    prefix = prefix.slice(1, prefix.length)
+  }
+  Object.keys(obj).map(k=>(typeof obj[k])==='string'?`<button>${k}</button>`:`<details><summary>${k}</summary>${ObjectToTree(obj[k]), prefix+'.'+k}</details>`)
 }
 function side() {
   document.getElementById('lang-select').innerHTML = `<option value="${main}" disabled>${main}</option>`+Object.keys(data).filter(l=>l!==main).map(l=>`<option value="${l}">${l}</option>`).sort().join('');
