2d4482e73f6d568fbd6b0ae9b502c087ccce9b05
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-23 16:48:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -43,10 +43,24 @@ function readFile(file) {
     reader.readAsText(file);
   });
 }
+function deleteCurrentLang() {
+  let current = document.getElementById('lang-select').value;
+  if (confirm('Are you sure you want to delete '+current)) {
+    delete data[current];
+    side();
+  }
+}
+function addLang() {
+  let code = prompt('Iso code of new language');
+  if (!code) return;
+  data[code] = {};
+  side();
+}
 function ObjectToTree(obj, prefix) {
   return Object.keys(obj).map(k=>(typeof obj[k])==='string'?`<button>${k}</button>`:`<details><summary>${k}</summary>${ObjectToTree(obj[k], (prefix.length?prefix+'.':'')+k)}</details>`).join('')
 }
 function side() {
+  if (!data[main]) return;
   document.getElementById('lang-select').innerHTML = `<option value="${main}" disabled>${main}</option>`+Object.keys(data).filter(l=>l!==main).map(l=>`<option value="${l}">${l}</option>`).sort().join('');
   document.getElementById('tree').innerHTML = ObjectToTree(data[main], '');
 }
@@ -57,24 +71,22 @@ var data = {};
 document.getElementById('file-load-button').addEventListener('click', ()=>{
   let files = document.getElementById('file').files;
   let type = document.getElementById('type-load').value;
-  main ??= prompt('Main language iso code (ej: en, es-ES...)', 'en') || 'en';
+  main ??= prompt('Main language iso code (ej: en, es-ES...)', 'en') ?? 'en';
   if (type === 'json-o') {
     for (let i = 0; i<files.length; i++) {
       readFile(files[i]).then(content => {
         let code = prompt('Iso code for: '+files[i].name, files[i].name.split('.')[0]);
         data[code] = parse(content, 'json');
-        if (code===main) {
-          side();
-        }
+        side();
       })
     }
   } else if (type === 'json-m') {
     readFile(files[0]).then(content => {
       let parsed = parse(content, 'json');
       Object.keys(parsed).forEach(l => {
-        data[l] = parsed[l]
+        data[l] = parsed[l];
+        side();
       })
-      side();
     })
   } else if (type === 'ftl') {
     // ftl
