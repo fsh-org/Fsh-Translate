43573aba41493101efa6f61622d8d197111d4ede
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-09-26 06:02:56 +0200

    Update ftl.js

--- media/ftl.js
+++ media/ftl.js
@@ -1,9 +1,25 @@
 function fromObjecrWithPrefix(obj, prefix) {
-  return 
+  return Object.keys(obj)
+    .filter(k=>k.length>1)
+    .map(k => {
+      if (typeof o[k] === 'string') {
+        return `${k} = ${o[k]}`;
+      } else {
+        return 'Objects in Objects not supported yet, sorry!'
+      }
+    }).join('\n');
 }
 
 function fromObject(obj) {
   return Object.keys(obj)
+    .filter(k=>k.length>1)
+    .map(k => {
+      if (typeof o[k] === 'string') {
+        return `${k} = ${o[k]}`;
+      } else {
+        return fromObjectWithPrefix(o[k], k)
+      }
+    }).join('\n');
 }
 
 function toObject(ftl) {}
