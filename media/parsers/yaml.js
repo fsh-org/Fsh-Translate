c414ce4a3fcf2680a89f54ebfb44559591dc4d29
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-07 22:54:01 +0200

    Update yaml.js

--- media/parsers/yaml.js
+++ media/parsers/yaml.js
@@ -2,7 +2,7 @@ function fromObjectWithPrefix(obj, prefix) {
   return Object.keys(obj)
     .map(k => {
       if (typeof obj[k] === 'object') {
-        return prefix+obj[k]+':\n'+fromObjectWithPrefix(obj[k], prefix+'  ');
+        return prefix+k+':\n'+fromObjectWithPrefix(obj[k], prefix+'  ');
       } else {
         return `${prefix}${k}: ${obj[k]}`;
       }
@@ -14,7 +14,7 @@ export function fromObject(obj) {
   return Object.keys(obj)
     .map(k => {
       if (typeof obj[k] === 'object') {
-        return obj[k]+':\n'+fromObjectWithPrefix(obj[k], '  ');
+        return k+':\n'+fromObjectWithPrefix(obj[k], '  ');
       } else {
         return `${k}: ${obj[k]}`;
       }
