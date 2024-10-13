9a1579337a013896670871230f8ea34645d947bb
inventionpro <109528211+inventionpro@users.noreply.github.com>
2024-10-13 21:11:55 +0200

    Update script.js

--- media/script.js
+++ media/script.js
@@ -97,7 +97,7 @@ window.setStringForLang = setStringForLang;
 function loadPanelFor(id) {
   let cur = document.getElementById('lang-select').value;
   document.querySelector('.panel').innerHTML = `<h2>${id.replaceAll('.',' > ')}</h2>
-<p>${getStringForLang(main??Object.keys(data)[0], id)}</p>
+<p>${getStringForLang(data[main]?main:Object.keys(data)[0], id)}</p>
 <hr>
 <textarea id="editor" class="editor" onkeyup="setStringForLang('${cur}', this.value, '${id}')">${getStringForLang(cur, id)}</textarea>`;
 }
